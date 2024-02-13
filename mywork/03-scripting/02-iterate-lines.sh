289b837bc2564f3c8a24bc723f7f3555e621221e
mackenziekeeley <143121480+mackenziekeeley@users.noreply.github.com>
2024-02-12 20:35:16 -0500

    committing

--- mywork/03-scripting/02-iterate-lines.sh
+++ mywork/03-scripting/02-iterate-lines.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+set -e
+
+while read line; do
+	echo $line;
+	# sleep !;
+done < guids.list
---
+++ mywork/03-scripting/readme.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# This script is designed to ____
+# Author: Mackenzie Keeley <spr2kv@virginia.edu>
+# Last updated: 2/6/24
+
+read -p "What is your favorite number?" FAVNUM
+echo "Hey $USER I heard your favorite number is $FAVNUM"
